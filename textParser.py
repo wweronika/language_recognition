03a6b27459e758bdf458683d8537f064c649ea21
Weronika Wiesiolek <wiesiolekweronika@gmail.com>
2020-09-14 12:04:16 +0200

    Changed network to KNN in scikit-learn, performing with 95% accuracy on 400 training entries and 100 test entries

--- textParser.py
+++ textParser.py
@@ -1,10 +1,11 @@
 import wikipedia
 import string
+import random
 
 class TextParser:
 
     def __init__(self):
-        pass
+        self.languages = ['en', 'fr', 'de', 'it']
 
     def readWiki(self, topic):
         page = wikipedia.page(topic)
@@ -12,10 +13,12 @@ class TextParser:
 
     def readRandomWiki(self):
         while True:
+            language = random.choice(self.languages)
+            wikipedia.set_lang(language)
             try:
                 random_topic = wikipedia.random(1)
                 page = wikipedia.page(random_topic)
-                return page.content
+                return [language, page.content]
             except wikipedia.exceptions.DisambiguationError as e:
                 continue
             except wikipedia.exceptions.PageError as e:
@@ -34,20 +37,17 @@ class TextParser:
                 c = char.lower()
                 if c in alphabet:
                     letterCount[c] += 1
-        print(letterCount)
         return letterCount
 
     # get fractional letter count
     def getNormalizedStats(self, text):
         letterCount = self.getLetterStats(text)
         nLetters = sum(letterCount.values())
-        print(nLetters)
         for key in letterCount.keys():
             letterCount[key] = letterCount[key] / nLetters
-        print(letterCount)
         return letterCount
 
 
-textParser = TextParser()
+# textParser = TextParser()
 # textParser.readRandomWiki()
-textParser.getNormalizedStats("acdfff")
+# textParser.getNormalizedStats("acdfff")
--- trainingEnv.py
+++ trainingEnv.py
@@ -1,42 +1,97 @@
 from textParser import TextParser
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+DATASET_SIZE = 500
 
 class TrainingEnvironment:
 
     def __init__(self):
         self.statistics = []
         self.languages = []
+        self.datasetSize = DATASET_SIZE
         self.textParser = TextParser()
-        self.languages = ['en', 'fr', 'de', 'it']
+        # self.network = self.initialiseNetwork()
 
-    def getPages(self, n):
-        for i in range(n):
-            self.pages.append(self.textParser.readRandomWiki())
+    def getDataset(self):
+        file_d = open("data.csv", "w+")
+        file_l = open("languages.csv", "w+")
+
+        # # first row in file_d is letters
+        # for letter in string.ascii_lowercase:
+        #     file_d.write(letter + ",")
+        # file_d.write("\n")
+
+        for i in range(self.datasetSize):
+
+            # get new Wiki text and its language
+            dataPair = self.textParser.readRandomWiki()
+            language = dataPair[0]
+            text = dataPair[1]
+            self.languages.append(language)
+            letterCount = self.textParser.getNormalizedStats(text)
+            self.statistics.append(letterCount)
+
+            # save to file
+            values = list(letterCount.values())
+            file_l.write(str(self.textParser.languages.index(language)) + "\n")
+            for j in range(len(values)):
+                if j == len(values) - 1:
+                    file_d.write(str(values[j]))
+                else:
+                    file_d.write(str(values[j])+",")
+            file_d.write("\n")
             print(i)
 
-    def getDataset(self, n):
-        for i in range(n):
-            text = self.textParser.readRandomWiki()
-            self.statistics.append(self.textParser.getNormalizedStats(text))
-            print(i)
-        dataset = np.array(self.statistics)
-        print(dataset)
-        return dataset
+    def loadData(self, file_l, file_d):
+        langs = np.genfromtxt(file_l, delimiter=",", dtype="int")
+        data = np.genfromtxt(file_d, delimiter=",", dtype="float")
+        # print(np.shape(data))
+        # print(langs)
+        return data, langs
+
 
     def initialiseNetwork(self):
 
         network = Sequential()
-        network.add(Dense(200, input_dim=26, activation='sigmoid'))
-        network.add(Dense(150, activation='sigmoid'))
-        network.add(Dense(100, activation='sigmoid'))
-        network.add(Dense(100, activation='sigmoid'))
-        network.add(Dense(len(self.languages), activation='softmax'))
+        # network.add(Dense(200, input_dim=26, activation='sigmoid'))
+        network.add(Dense(500,input_dim=26, kernel_initializer="glorot_uniform", activation="sigmoid"))
+        network.add(Dropout(0.5))
+        network.add(Dense(300, kernel_initializer="glorot_uniform", activation="sigmoid"))
+        network.add(Dropout(0.5))
+        network.add(Dense(100, kernel_initializer="glorot_uniform", activation="sigmoid"))
+        network.add(Dropout(0.5))
+        network.add(Dense(1, activation='linear'))
 
-        network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        network.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
         return network
 
+    # data - x, languages - y
+    def trainNetwork(self, X, Y):
+        pass
+        # self.network.fit(X, Y, epochs=30, batch_size=1)
+
+    def knn(self, X, Y, test_size):
+        neigh = KNeighborsClassifier(n_neighbors=3)
+        neigh.fit(data[test_size:], langs[test_size:])
+        predictions = neigh.predict(data[:test_size])
+        print(predictions)
+        print(langs[:test_size] - predictions)
+        errors = 0
+        for entry in langs[:test_size] - predictions:
+            if entry != 0:
+                errors += 1
+        accuracy = (len(predictions) - errors) / len(predictions) * 100
+        print(str(accuracy)+"%")
+
 tEnv = TrainingEnvironment()
-tEnv.getDataset(20)
+# tEnv.getDataset()
+data, langs = tEnv.loadData("languages.csv", "data.csv")
+# tEnv.trainNetwork(data, langs)
+tEnv.knn(data, langs, 100)
+
